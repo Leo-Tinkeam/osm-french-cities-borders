58c7b561389233958a18542140485b989824c7e6
Léo-Tinkeam <sarcyleo@gmail.com>
2026-03-29 17:55:15 +0200

    Generate Intermediate Dict

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Download data from geofabrick (restriction of https://planet.openstreetmap.org/ for France)
 
 > uv run download_france_binary.py
+
+# Generate intermediate file that contains data that can be read faster for our objective
+
+> uv run generate_france_dict.py
---
+++ generate_france_dict.py
@@ -0,0 +1,86 @@
+import osmium
+
+binary_file_name = "france-latest.osm.pbf"
+
+class City:
+    def __init__(self, name: str, border):
+        self.name: str = name
+        self.border: Border = border
+
+class Border:
+    def __init__(self, related_ways):
+        self.related_objects: list[WayDescriptor] = related_ways
+
+class WayDescriptor:
+    def __init__(self, id, role):
+        self.id: int = id
+        self.role: str = role
+
+cities: dict[int, City] = {}
+ways = {}
+nodes = {}
+
+class CityHandler(osmium.SimpleHandler):
+    def relation(self, r):
+        tags = r.tags
+        if (tags.get("type") == "boundary" and
+            tags.get("boundary") == "administrative" and
+            tags.get("admin_level") == "8"):
+            
+            border = Border([WayDescriptor(m.ref, m.role) for m in r.members])
+            cities[r.id] = City(name=tags.get("name"), border=border)
+
+handler = CityHandler()
+handler.apply_file(binary_file_name)
+print("Number of cities :", len(cities))
+
+way_id_needed: set[int] = set()
+for key, value in cities.items():
+    for way in value.border.related_objects:
+        way_id_needed.add(way.id)
+print("Way_Id needed :", len(way_id_needed))
+
+class WayCollector(osmium.SimpleHandler):
+    def __init__(self):
+        super().__init__()
+        self.count = 0
+
+    def way(self, w):
+        if w.id in way_id_needed:
+            ways[w.id] = [n.ref for n in w.nodes]
+            self.count += 1
+            if self.count == 10000:
+                print("Number of ways :", len(ways), "of", len(way_id_needed), "(predicted)")
+                self.count = 0
+
+handler = WayCollector()
+handler.apply_file(binary_file_name, locations=False)
+print("End of ways processing !", len(ways), "ways in total")
+
+node_id_needed = set()
+for key, value in ways.items():
+    for id in value:
+        node_id_needed.add(id)
+print("Node_Id needed :", len(node_id_needed))
+
+class NodeCollector(osmium.SimpleHandler):
+    def __init__(self):
+        super().__init__()
+        self.count = 0
+
+    def node(self, n):
+        if n.id in node_id_needed:
+            nodes[n.id] = (n.location.lon, n.location.lat)
+            self.count += 1
+            if self.count == 100000:
+                print("Number of nodes :", len(nodes), "of", len(node_id_needed))
+                self.count = 0
+
+handler = NodeCollector()
+handler.apply_file(binary_file_name, locations=True)
+print("End of nods processing!", len(nodes), "nodes in total")
+
+import pickle
+france_dict = {"cities": cities, "ways": ways, "nodes": nodes}
+with open('france_dict.pkl', 'wb') as f:
+    pickle.dump(france_dict, f)
